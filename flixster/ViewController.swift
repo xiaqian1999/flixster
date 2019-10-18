968729b316b767ab580cdc94c00a7dbcf680bb4e
xiaqian <xiaqian1999@gmail.com>
2019-10-18 13:43:58 -0400

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by 潘玉霖 on 10/18/19.
+//  Copyright © 2019 xiaqian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
